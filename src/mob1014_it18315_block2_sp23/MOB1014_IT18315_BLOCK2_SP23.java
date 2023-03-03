b613745b2add51778767b9e28496857bd0c00f77
HangNT169 <HangNT169@fpt.edu.vn>
2023-03-04 01:46:13 +0700

    Init

---
+++ src/mob1014_it18315_block2_sp23/MOB1014_IT18315_BLOCK2_SP23.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mob1014_it18315_block2_sp23;
+
+/**
+ *
+ * @author hangnt
+ */
+public class MOB1014_IT18315_BLOCK2_SP23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
